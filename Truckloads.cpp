d516c370445e6405277ff5959ef8e16909ab9aab
a1837203 <100544496+a1837203@users.noreply.github.com>
2022-04-04 22:21:19 +0800

    Update Truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,15 +1,1 @@
-#include "Truckloads.h"
 
-       int Trucklads::Truckloads_Number(int val)
-
-       {
-
-           if(val == 0) return 0;
-
-       if(val == 1 || val == 2) return 1;
-
-       return Truckloads_Number(val-1) + Truckloads_Number(val-2);
-
-       }
-
-      
