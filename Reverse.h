6c88ac5392fea0b389a4a361a8092e2a90e67af7
a1837203 <100544496+a1837203@users.noreply.github.com>
2022-04-04 15:07:58 +0800

    Add files via upload

---
+++ Reverse.h
@@ -0,0 +1,20 @@
+#include <string>
+
+
+    #include <math.h>
+
+    using namespace std;
+
+    class Reverse
+
+    {
+
+       public:
+
+       string reverseString(string str1);
+
+       int reverse_Digit(int val);
+
+     
+
+    };
